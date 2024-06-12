b499327ae1e15391ad3e830d049197e372bb1ebe
Vasilevykh-M <vasilevyh00@gmail.com>
2024-06-12 22:14:40 +0500

    add serv

--- bacrkround_remover.py
+++ bacrkround_remover.py
@@ -7,15 +7,27 @@ import torch
 class RemoverKand():
   def __init__(self):
     self.remover = Remover(mode='base')
-  def resize_with_padding(self, img, expected_size):
-      img.thumbnail((expected_size[0], expected_size[1]))
-      delta_width = expected_size[0] - img.size[0]
-      delta_height = expected_size[1] - img.size[1]
-      pad_width = delta_width // 2
-      pad_height = delta_height // 2
-      padding = (pad_width, pad_height, delta_width - pad_width, delta_height - pad_height)
-      return ImageOps.expand(img, padding)
+  def resize_with_padding(self, img, type_img):
+      new_w = img.size[0] * 2
+      new_h = img.size[1] * 2
+      border_h = img.size[1]
+      border_w = img.size[0]
+      img.thumbnail((new_w, new_h))
+      border_pad = 0
+      if type_img == 1:
+          border_pad = 0.75
+      if type_img == 2:
+          border_pad = 0.5
+      if type_img == 3:
+          border_pad = 0
 
-  def remove_backgroud(self, img):
-    img = self.resize_with_padding(img, (512, 512))
+      return ImageOps.expand(img, border=(
+          border_w,
+          border_h - (int)(border_pad * border_h),
+          border_w,
+          border_h + (int)(border_pad * border_h)
+      ), fill=(0, 0, 0, 0))
+
+  def remove_backgroud(self, img, type_img):
+    img = self.resize_with_padding(img, type_img)
     return self.remover.process(img, type='map'), img
--- clip.py
+++ clip.py
@@ -23,6 +23,14 @@ class ClipModel:
     def __call__(self, image):
 
         logits_per_image, logits_per_text = self.model(image, self.text)
+        idx = torch.argmax(logits_per_image.softmax(dim=-1)).item()
         probs = self.data_label[torch.argmax(logits_per_image.softmax(dim=-1)).item()]
-        return probs
+        img_group = 0
+        if idx == 0 or idx == 1 or idx == 5:
+            img_group = 0
+        if idx == 2 or idx == 3 or idx == 4:
+            img_group = 1
+        if idx == 6 or idx == 7 or idx == 8 or idx == 9 or idx == 10:
+            img_group = 2
+        return probs, img_group
 
