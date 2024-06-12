5c008e4f52c1a287055b8bac365dec9f7ad26fcd
Danil Vagapov <adast@yahoo.com>
2024-06-13 01:42:03 +0500

    add scale support to resize_with_padding

--- bacrkround_remover.py
+++ bacrkround_remover.py
@@ -5,23 +5,32 @@ from transparent_background import Remover
 class RemoverKand():
     def __init__(self):
         self.remover = Remover(mode='base')
-    def resize_with_padding(self, img, type_img):
-        border_h = (1.5 * (int)(max(img.size[0], img.size[1]) * 1.5 / 2) - img.size[1]) // 2
-        border_w = (1.5 * (int)(max(img.size[0], img.size[1]) * 1.5 / 2) - img.size[0]) // 2
-        border_pad = 0
+
+    def resize_with_padding(img, type_img):
+        max_size = max(img.size[1], img.size[0])
+        border_h = (max_size - img.size[1]) // 2
+        border_w = (max_size - img.size[0]) // 2
+
+        scale = 0.75
+        border_pad = 0.
         if type_img == 1:
+            scale = 0.25
             border_pad = 0.75
         if type_img == 2:
+            scale = 0.5
             border_pad = 0.25
         if type_img == 3:
+            scale = 0.75
             border_pad = 0
 
+        border_h += int((1 - scale) * max_size)
+        border_w += int((1 - scale) * max_size)
+
         return ImageOps.expand(img, border=(
-        border_w,
-        border_h - (int)(border_pad * border_h),
-        border_w,
-        border_h + (int)(border_pad * border_h)
-        ), fill=(0, 0, 0, 0))
+            border_w,
+            border_h - (int)(border_pad * border_h),
+            border_w,
+            border_h + (int)(border_pad * border_h)))
 
     def remove_backgroud(self, img, type_img):
         img = self.resize_with_padding(img, type_img)
